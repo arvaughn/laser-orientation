3762fed1390ca7825be8c5d53f30f184c1036010
arvaughn <108916113+arvaughn@users.noreply.github.com>
2022-07-12 11:05:40 -0400

    LASER Day 2 AM R Script

---
+++ Vaughn laser-orientation code along.R
@@ -0,0 +1,32 @@
+round(3.14, digits = 1)
+twenty <-20
+twenty
+thirty <- 20
+thirty
+forty <- 20
+forty
+new_data <- 20
+new_data
+13 <- 20 *4
+13
+x <- 20 *4
+x
+my_number <- 13
+my_number
+my_product <- my_number * 2
+
+my_product
+library(tidyverse)
+?tidyverse
+library(readr)
+?readr
+do_this(to_this)  
+do_that(to_this, to_that, with_those)
+add_numbers <- function(number_1, number_2) {
+  number_1 + number_2
+}
+add_numbers(4,6)
+library(readr)
+sci_data <- read_csv("data/sci-online-classes.csv")
+View(sci_data)
+ggplot(sci_data, aes(x = FinalGradeCEMS)) + geom_histogram(fill = "blue")
